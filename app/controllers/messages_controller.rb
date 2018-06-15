6192ab91531bd06f260c3406f9bdc3718b4da81a
Nino Prekratic <ninoprekratic@gmail.com>
2018-06-15 18:59:43 +0200

    Messages are stored and shown
    Messages are stored in the database and can be shown individually on the screen, or all together.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,27 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all.order("created_at DESC")
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(post_params)
+    if @message.save 
+      redirect_to @message
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:message).permit(:body)
   end
 end
